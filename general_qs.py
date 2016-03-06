9c91dc17ca2e889e19fe997bb68b98e73ad88e8d
Anya Johnson <anyaejo@gmail.com>
2016-03-06 14:03:00 -0500

    added selection calculator straight in and separated metabolism and reproduction

--- general_qs.py
+++ general_qs.py
@@ -8,7 +8,7 @@ import copy
 
 class Organism:
   '''A class to contain an organism'''
-  def __init__(self, cellID, ai_prob = 0.5, coop_prob = 0.5, neigh_prop = 0.6, parent=None, empty=False, lineage = -1):
+  def __init__(self, cellID, ai_prob = 0.5, coop_prob = 0.5, neigh_prop = 0.6, parent=None, empty=False, lineage = 'evolvable'):
     self.age = 0
     self.generation = 0
     self.ai_prob = ai_prob
@@ -39,6 +39,7 @@ class Organism:
         self.ai_produce = (random.random() < self.ai_prob)
         if self.ai_produce:
           self.points = signal_cost
+          
         
   def __repr__(self):
     # Using string formatting
@@ -49,6 +50,7 @@ class Organism:
     if not self.empty:
       self.age += 1
       self.points += 1
+      population_orgs.select_dict[self.lineage] += 1
       if (random.random() < self.coop_prob):
         #organism will test whether there are enough ai neighbors to produce
         neighbors = self.findNeighbors()
@@ -57,10 +59,9 @@ class Organism:
           if population_orgs.orgs[neighbor_id].ai_produce:
             neighbor_ai += 1
         if (neighbor_ai/8.0) >= self.neigh_prop:
-          #produce the public good!
           self.points += production_cost
-          for neighbor_id in neighbors:
-            population_orgs.orgs[neighbor_id].points += public_good_points
+          #tell the population that public good was produced and should be given to neighbors
+          return neighbors
           
       
 
@@ -117,6 +118,9 @@ class Population:
     self.currentUpdate = 0
     self.orgs = []
     self.pop_size = popsize
+    #dictionary to count the bonuses and costs for each type of organism
+    self.select_dict = {'defector':0, 'wt':0, 'uncond':0, 'empty':0}
+
     
     non_empty = 0
     for i in range(popsize):
@@ -133,6 +137,7 @@ class Population:
       else:
         self.orgs.append(self.makeOrg("empty"))
         non_empty -=1
+      self.select_dict[self.orgs[-1].lineage] += self.orgs[-1].points
                       
     if non_empty < 1:
       #seed with an organism from the group with highest proportion or the ancestor if all are zero
@@ -141,6 +146,7 @@ class Population:
         self.orgs[0] = self.makeOrg("ancestor")
       else:
         self.orgs[0]= (self.makeOrg(max_key))
+      self.select_dict[self.orgs[-1].lineage] += self.orgs[-1].points
                         
 
 
@@ -151,18 +157,18 @@ class Population:
     unconditional = AI 0 Coop_Prob 0.1 Neigh_Prop 0
     defector = AI 0 Coop_Prob 0.0 Neigh_Prop 1.0'''
     if org_type == "ancestor":
-      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 0.0)
+      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 0.0, lineage='ancestor')
     elif org_type == "wt":
-      newOrg = Organism(len(self.orgs), ai_prob = 1.0, coop_prob = 0.5, neigh_prop = 0.6)
+      newOrg = Organism(len(self.orgs), ai_prob = 1.0, coop_prob = 0.5, neigh_prop = 0.6, lineage='wt')
     elif org_type == "unconditional":
-      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.5, neigh_prop = 0.0)
+      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.5, neigh_prop = 0.0, lineage='uncond')
     elif org_type == "defector":
-      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 1.0)
+      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 1.0, lineage = 'defector')
     elif org_type == "empty":
-      newOrg= Organism(len(self.orgs), empty = True)
+      newOrg= Organism(len(self.orgs), empty = True, lineage = 'empty')
     else:
       print "Invalid organism selected, making ancestor"
-      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 0.0)
+      newOrg = Organism(len(self.orgs), ai_prob = 0.0, coop_prob = 0.0, neigh_prop = 0.0, lineage = 'ancestor')
     return newOrg
 
   def reproduceOrg(self, org):
@@ -182,6 +188,8 @@ class Population:
     newOrg = Organism(position, parent = org)
 
     self.orgs[position] = newOrg
+    self.select_dict[newOrg.lineage] += newOrg.points
+
 
   def update(self):
     '''A function that runs a single update'''
@@ -189,6 +197,16 @@ class Population:
     for org in self.orgs:
       if not org.empty:
         result = org.update()
+      if result: #organism produced public good and needs to give to neighbors
+        assert isinstance(result, list), "Result returned by organism must be a list"
+        #track the decrease to the producer
+        self.select_dict[org.lineage] += production_cost
+        for neighbor_id in result:
+          neighbor = self.orgs[neighbor_id]
+          self.select_dict[neighbor.lineage] += public_good_points
+          neighbor.points += public_good_points
+
+    for org in self.orgs:
         if org.points >= 10:
           self.reproduceOrg(org)
 
@@ -212,23 +230,28 @@ class Population:
     else:
       return (ai_prob_sum/total), (coop_prob_sum/total), (neigh_prop_sum/total), (repo_age_sum/total), total
 
+def parseArgs(parser):
+  '''A function for parsing the arguments.'''
+  parser.add_argument('pop_x', help="the x value for the population size", type=int)
+  parser.add_argument('pop_y', help="the y value for the population size", type=int)
+  parser.add_argument('seed', help="the random seed", type=int)
+  parser.add_argument('--mutation_sd', help='set the standard deviation of mutations, 0 to turn off mutations, 0.1 by default', type=float, default=0.1)
+  parser.add_argument('filename', help='the prefix filename to save data to')
+  parser.add_argument('--proportion_wt', help="the starting proportion of the population that should be the wildtype organism", type=float, default=0.0)
+  parser.add_argument('--proportion_def', help="the starting proportion of the population that should be the defector organism", type=float, default=0.0)
+  parser.add_argument('--proportion_uncond', help="the starting proportion of the population that should be the unconditional cooperator organism", type=float, default=0.0)
+  parser.add_argument('--proportion_ancestor', help="the starting proportion of the population that should be the ancestor organism", type=float, default=0.0)
+  parser.add_argument('--signal_cost', help="the cost of using autoinducers, should be a negative number", type=float, default=-1.0)
+  parser.add_argument('--production_cost', help="the cost of producing the public good, should be negative", type=float, default=-5.0)
+  parser.add_argument('--public_good_points', help='the bonus neighbors are rewarded when an organism produces the public good, should be positive', type=float, default=4.9)
+  parser.add_argument('--num_updates', help="the number of updates the experiment should run for", type=int, default=10000)
+  args = parser.parse_args()
+  return args
+
+
 
 parser = argparse.ArgumentParser(description='Simulate evolution of quorum sensing controlled public goods')
-parser.add_argument('pop_x', help="the x value for the population size", type=int)
-parser.add_argument('pop_y', help="the y value for the population size", type=int)
-parser.add_argument('seed', help="the random seed", type=int)
-parser.add_argument('--mutation_sd', help='set the standard deviation of mutations, 0 to turn off mutations, 0.1 by default', type=float, default=0.1)
-parser.add_argument('filename', help='the prefix filename to save data to')
-parser.add_argument('--proportion_wt', help="the starting proportion of the population that should be the wildtype organism", type=float, default=0.0)
-parser.add_argument('--proportion_def', help="the starting proportion of the population that should be the defector organism", type=float, default=0.0)
-parser.add_argument('--proportion_uncond', help="the starting proportion of the population that should be the unconditional cooperator organism", type=float, default=0.0)
-parser.add_argument('--proportion_ancestor', help="the starting proportion of the population that should be the ancestor organism", type=float, default=0.0)
-parser.add_argument('--signal_cost', help="the cost of using autoinducers, should be a negative number", type=float, default=-1.0)
-parser.add_argument('--production_cost', help="the cost of producing the public good, should be negative", type=float, default=-5.0)
-parser.add_argument('--public_good_points', help='the bonus neighbors are rewarded when an organism produces the public good, should be positive', type=float, default=-4.9)
-parser.add_argument('--num_updates', help="the number of updates the experiment should run for", type=int, default=10000)
-args = parser.parse_args()
-
+args = parseArgs(parser)
 seed = args.seed
 random.seed(seed)
 numpy.random.seed(seed)
@@ -244,7 +267,7 @@ public_good_points = args.public_good_points
 proportions = {'wt':args.proportion_wt, 'defector':args.proportion_def, 'unconditional':args.proportion_uncond, 'ancestor':args.proportion_ancestor}
 
 data_file = open(filename+str(seed)+".dat", 'w')
-data_file.write("Update AI_Prob Coop_Prob Num_Neigh Avg_Repo_Age Total\n")
+data_file.write("Update AI_Prob Coop_Prob Num_Neigh Avg_Repo_Age Total Select_Def Select_WT Select_Uncond\n")
 data_file.close()
 population_orgs = Population(pop_size,proportions)
 
@@ -257,7 +280,8 @@ for u in range(num_updates):
     ai_prob, coop_prob, neigh_prop, age_avg, total = population_orgs.recordStats()
     print "Update: ", u, " AI Prob: ", ai_prob, " Coop Prob: ", coop_prob, " Neighbor Prop: ", neigh_prop, " Avg Repo Age: ", age_avg, " Total Orgs: ", total
     data_file = open(filename+str(seed)+".dat", 'a')
-    data_file.write('{} {} {} {} {} {}\n'.format(u, ai_prob, coop_prob, neigh_prop, age_avg, total))
+    data_file.write('{} {} {} {} {} {} {} {} {}\n'.format(u, ai_prob, coop_prob, neigh_prop, age_avg, total, population_orgs.select_dict['defector'], population_orgs.select_dict['wt'], population_orgs.select_dict['uncond']))
+    population_orgs.select_dict = population_orgs.select_dict.fromkeys(population_orgs.select_dict, 0)
     data_file.close()
     if total == 0:
       break
